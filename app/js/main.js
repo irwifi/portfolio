c1e3a4b2d0d9e14f1e12f84e7996810ef3aa027d
Raju Malla <irwifi@gmail.com>
2017-01-27 13:48:21 +0545

    Block Internet explorer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
-$(
-	() => {
-		$(".not_ready").remove()
-	}
-)
+// Initialize the page
+const init = () => {
+	// Remove the noscript block
+	$("noscript").remove()
+
+	// Remove IE block script and messages
+	$("#detect_ie").remove()
+	$(".ie_block_script").remove()
+
+	// Remove the loading screen once the page is ready
+	$(".not_ready").remove()
+		
+	// Make the avatar appear
+	$(".avatar_container #avatar_img").animate({width: '400px', height: '400px'}, 800, avatar_init_ready)
+	$(".avatar_container #avatar_img").fadeTo(800, 1)	
+}
+
+// Initialize the objects  when page is ready
+$(init)
+
+const avatar_init_ready = () => {
+}
